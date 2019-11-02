03b65432880b674b591e15f57ffbcbeb257f4a3d
Cosmin Dimisca <cosmin.dimisca@mindit.io>
2019-11-02 16:32:06 +0200

    post message start

--- src/app/components/page-mirror/page-mirror.component.ts
+++ src/app/components/page-mirror/page-mirror.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-mirror',
   templateUrl: './page-mirror.component.html',
   styleUrls: ['./page-mirror.component.css']
 })
-export class PageMirrorComponent implements OnInit {
+export class PageMirrorComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('iframe', {static: true}) iframe: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit() {
+  }
+
+  getIframeDOM(){
+    if(this.iframe.nativeElement){
+     var wn = this.iframe.nativeElement.contentWindow;
+     wn.postMessage('Hello to iframe from parent!', 'http://localhost:3000');
+  }
+
+}
 
 }
