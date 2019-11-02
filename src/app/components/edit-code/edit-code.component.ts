681d093da281bd37e2814f0bb7ef83c5f02977ef
Cosmin Dimisca <cosmin.dimisca@mindit.io>
2019-11-02 19:06:32 +0200

    Added service

--- src/app/components/edit-code/edit-code.component.ts
+++ src/app/components/edit-code/edit-code.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ParserService} from '../../services/parser.service';
 
 @Component({
   selector: 'app-edit-code',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCodeComponent implements OnInit {
 
-  constructor() { }
+  styles: any;
+
+  constructor(private parserService: ParserService) { }
 
   ngOnInit() {
+    this.styles = this.parserService.element.target.style;
+    console.log(this.styles);
   }
 
 }
